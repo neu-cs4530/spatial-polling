bd19dfca563f0dc68eb84d2c5f465ac229a86d59
ericnich <61976685+ericnich@users.noreply.github.com>
2022-03-25 12:29:25 -0400

    Linter Compliance
    1. Moved GridSquare to its own file.
    2. Spaced out brackets.
    3. Added a semicolon.

--- frontend/src/classes/BoundingBox.ts
+++ frontend/src/classes/BoundingBox.ts
@@ -41,17 +41,3 @@ export default class BoundingBox {
   }
 
 }
-
-/**
- * This extends the BoundingBox class. It stores the result of toRectPoints() for convenience.
- * @param box a dictionary with the coordinates of the top left and bottom right corners of the BoundingBox
- */
-export class GridSquare extends BoundingBox {
-  public box;
-
-  constructor(x: number, y: number, width: number, height: number) {
-    super(x, y, width, height);
-    this.box = this.toRectPoints;
-  }
-  
-}
--- services/townService/src/client/TownsServiceClient.ts
+++ services/townService/src/client/TownsServiceClient.ts
@@ -14,7 +14,7 @@ export type BoundingBox = {
   y: number,
   width: number,
   height: number
-  toRectPoints(): {x1: number, y1: number, x2: number, y2: number};
+  toRectPoints(): { x1: number, y1: number, x2: number, y2: number };
 };
 export type ServerConversationArea = {
   label: string;
@@ -27,8 +27,8 @@ export type ServerConversationArea = {
  * A bounding box with its location also stored as corner coordinates
  */
 export type GridSquare = BoundingBox & {
-  box: {x1: number, y1: number, x2: number, y2: number};
-}
+  box: { x1: number, y1: number, x2: number, y2: number };
+};
 
 /**
  * The format of a request to join a Town in Covey.Town, as dispatched by the server middleware

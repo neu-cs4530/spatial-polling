a8f9d5385734a222508d25386e8379e66547c82b
Justin Rosario <rosario.ju@northeastern.edu>
2022-04-19 13:24:49 -0400

    Added basic poll info to modals, listOptions function to create list items with colors

--- frontend/src/components/world/ActivePollView.tsx
+++ frontend/src/components/world/ActivePollView.tsx
@@ -10,6 +10,17 @@ export type ActivePollViewProps = {
     poll: ConversationAreaPoll;
 }
 
+const colorsArray: string[] = ['red', 'blue', 'green', 'yellow']
+
+export function listOptions(apvProps: ActivePollViewProps): JSX.Element[] {
+    
+    var optionsList: JSX.Element[] = [];
+    apvProps.poll.options.forEach((option, index) => {
+        optionsList.push(<li id={index.toString()}><span style={{color: colorsArray[index]}}>■</span> {option}</li>)
+    })
+    return optionsList;
+}
+
 export default function ActivePollView(apvProps: ActivePollViewProps): JSX.Element {
     if (apvProps.player.id === apvProps.poll.creatorID) {
         return ActivePollViewCreator(apvProps);
--- frontend/src/components/world/ActivePollViewVoter.tsx
+++ frontend/src/components/world/ActivePollViewVoter.tsx
@@ -1,6 +1,28 @@
-import { Modal } from "@chakra-ui/react";
-import { ActivePollViewProps } from "./ActivePollView";
+import { Modal, ModalBody, ModalCloseButton, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import useMaybeVideo from "../../hooks/useMaybeVideo";
+import { ActivePollViewProps, listOptions } from "./ActivePollView";
 
 export default function ActivePollViewVoter(apvProps: ActivePollViewProps) {
-    return <Modal />
+    const video = useMaybeVideo()
+    return (
+        <Modal isOpen={apvProps.isOpen} onClose={()=>{apvProps.closeModal(); video?.unPauseGame;}}>
+            <ModalOverlay/>
+            <ModalHeader>
+                <text>
+                    Poll Information
+                </text>
+            </ModalHeader>
+            <ModalBody pb={6}>
+                <text>
+                    <b>Prompt:</b> {apvProps.poll.prompt}
+                </text>
+                <text>
+                    <b>Poll Options:</b>
+                </text>
+                <ul>
+                    {listOptions(apvProps)}
+                </ul>
+            </ModalBody>
+            <ModalCloseButton/>
+        </Modal>)
 }

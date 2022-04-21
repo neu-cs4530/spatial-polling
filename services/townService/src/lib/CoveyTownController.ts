7b7661290d27de44e3988a22a5fb9f2f22cf511f
Caroline Hughes <carolinehughes@Carolines-MacBook-Pro.local>
2022-04-21 13:53:05 -0400

    rest

--- services/townService/src/lib/CoveyTownController.ts
+++ services/townService/src/lib/CoveyTownController.ts
@@ -123,7 +123,9 @@ export default class CoveyTownController {
     const conversation = session.player.activeConversationArea;
     if (conversation) {
       if (conversation.activePoll) {
-        conversation.activePoll.options.forEach(o => {o.voters = o.voters.filter(v => {v !== session.player.id})});
+        conversation.activePoll.options.forEach(o => {
+          o.voters = o.voters.filter(v => v !== session.player.id);
+        });
       }
       this.removePlayerFromConversationArea(session.player, conversation);
     }
